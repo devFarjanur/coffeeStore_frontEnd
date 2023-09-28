86de9eb2c30815eda22678e4bafff81e49a22eb6
devFarjanur <farjanur15-2956@diu.edu.bd>
2023-09-28 15:45:59 +0600

    card updated

--- src/Components/CoffeeCard/CoffeeCard.jsx
+++ src/Components/CoffeeCard/CoffeeCard.jsx
@@ -11,8 +11,12 @@ const CoffeeCard = ({coffee}) => {
             <div className="card card-side bg-base-100 shadow-xl">
                 <figure><img src={photo} alt="Movie" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">New movie is released!</h2>
-                    <p>Click the button to watch on Jetflix app.</p>
+                    <h2 className="card-title">{name}</h2>
+                    <p>{quantity}</p>
+                    <p>{supplire}</p>
+                    <p>{taste}</p>
+                    <p>{category}</p>
+                    
                     <div className="card-actions justify-end">
                         <button className="btn btn-primary">Watch</button>
                     </div>
